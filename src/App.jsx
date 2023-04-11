10f0dd31afa033cf3d709c5aa4db9d22915e9cd4
DMould123 <95115610+DMould123@users.noreply.github.com>
2023-04-11 08:43:48 +0200

    Very basic styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,70 @@
-import { useState } from 'react';
+  import { useState } from 'react';
+  import './styles.css';
 
-export default function App() {
-  const [newTask, setNewTask] = useState('')
-  const [tasks, setTasks] = useState([])
 
-  function handleSubmit(e) {
-    e.preventDefault()
+  export default function App() {
+    const [newTask, setNewTask] = useState('')
+    const [tasks, setTasks] = useState([])
 
-    setTasks(ongoingTasks => {
-      return [... ongoingTasks, {
-        id: crypto.randomUUID(), title: newTask, completed: false
-      }]
-    })
+    function handleSubmit(e) {
+      e.preventDefault()
 
-    setNewTask(''); // clear the input field after adding the task
-  }
+      setTasks(ongoingTasks => {
+        return [... ongoingTasks, {
+          id: crypto.randomUUID(), title: newTask, completed: false
+        }]
+      })
 
-  function handleToggleComplete(taskId) {
-    setTasks(tasks => {
-      return tasks.map(task => {
-        if (task.id === taskId) {
-          return { ...task, completed: !task.completed };
-        } else {
-          return task;
-        }
+      setNewTask(''); // clear the input field after adding the task
+    }
+
+    function handleToggleComplete(taskId) {
+      setTasks(tasks => {
+        return tasks.map(task => {
+          if (task.id === taskId) {
+            return { ...task, completed: !task.completed };
+          } else {
+            return task;
+          }
+        });
       });
-    });
+    }
+
+    function handleDeleteTask(taskId) {
+      setTasks(ongoingTasks => {
+        return ongoingTasks.filter(task => task.id !== taskId);
+      });
+    }
+
+    return (
+      <>
+        <form onSubmit={handleSubmit} className="new-action-form">
+          <div className="new-action-form-row">
+            <label htmlFor="task">Task List</label>
+            <input value={newTask} onChange={e => setNewTask(e.target.value)} type="text" id='task' />
+          </div>
+          <button className='btn'>Add Task</button>
+        </form>
+
+        <h1 className='header'> Task List</h1>
+        <ul className='task-list'>
+          {tasks.length === 0 && "There are currently no remaining tasks"}
+          {tasks.map(task => (
+            <li key={task.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={task.completed}
+                  onChange={() => handleToggleComplete(task.id)}
+                />
+                {task.title}
+              </label>
+              <button className='btn btn-danger' onClick={() => handleDeleteTask(task.id)}>
+  <i className='fas fa-trash'></i>
+</button>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
   }
-
-  function handleDeleteTask(taskId) {
-    setTasks(ongoingTasks => {
-      return ongoingTasks.filter(task => task.id !== taskId);
-    });
-  }
-
-  return (
-    <>
-      <form onSubmit={handleSubmit} className="new-action-form">
-        <div className="new-action-form-row">
-          <label htmlFor="task">Task List</label>
-          <input value={newTask} onChange={e => setNewTask(e.target.value)} type="text" id='task' />
-        </div>
-        <button className='btn'>Add Task</button>
-      </form>
-
-      <h1 className='header'> Task List</h1>
-      <ul className='task-list'>
-        {tasks.length === 0 && "There are currently no remaining tasks"}
-        {tasks.map(task => (
-          <li key={task.id}>
-            <label>
-              <input
-                type="checkbox"
-                checked={task.completed}
-                onChange={() => handleToggleComplete(task.id)}
-              />
-              {task.title}
-            </label>
-            <button className='btn btn-danger' onClick={() => handleDeleteTask(task.id)}> Delete</button>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-}
