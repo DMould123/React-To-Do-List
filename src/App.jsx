0d31c2027ae2a8995f47782c6edacdf89d754c03
DMould123 <95115610+DMould123@users.noreply.github.com>
2023-08-02 11:54:18 +0200

    Added a priority selector to the task and a sorting filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 
 export default function App() {
   const [newTask, setNewTask] = useState('');
   const [tasks, setTasks] = useState([]);
   const [filter, setFilter] = useState('all');
+  const [sort, setSort] = useState('none'); // Sorting state, options: none, priority, dueDate, creationDate
+  const [showArchived, setShowArchived] = useState(false);
 
   // Load tasks from LocalStorage on app startup
   useEffect(() => {
@@ -23,9 +25,16 @@ export default function App() {
     e.preventDefault();
 
     setTasks(ongoingTasks => {
-      return [...ongoingTasks, {
-        id: crypto.randomUUID(), title: newTask, completed: false, dueDate: null
-      }];
+      return [
+        ...ongoingTasks,
+        {
+          id: crypto.randomUUID(),
+          title: newTask,
+          completed: false,
+          dueDate: null,
+          priority: 'medium', // default priority is medium
+        },
+      ];
     });
 
     setNewTask(''); // clear the input field after adding the task
@@ -73,11 +82,39 @@ export default function App() {
     });
   }
 
+  function handlePriorityChange(taskId, newPriority) {
+    setTasks(tasks => {
+      return tasks.map(task => {
+        if (task.id === taskId) {
+          return { ...task, priority: newPriority };
+        } else {
+          return task;
+        }
+      });
+    });
+  }
+
+  // Function to handle task sorting
+  const sortedTasks = tasks.slice().sort((a, b) => {
+    if (sort === 'none') return 0;
+    if (sort === 'priority') {
+      const priorityOrder = { high: 3, medium: 2, low: 1 };
+      return priorityOrder[b.priority] - priorityOrder[a.priority];
+    }
+    if (sort === 'dueDate') {
+      return (a.dueDate || '') > (b.dueDate || '') ? 1 : -1;
+    }
+    if (sort === 'creationDate') {
+      return b.id.localeCompare(a.id);
+    }
+    return 0;
+  });
+
   // Function to handle task filtering
-  const filteredTasks = tasks.filter((task) => {
+  const filteredTasks = sortedTasks.filter(task => {
     if (filter === 'all') return true;
-    if (filter === 'completed') return task.completed;
-    if (filter === 'remaining') return !task.completed;
+    if (filter === 'completed') return task.completed && (!showArchived || task.archived);
+    if (filter === 'remaining') return !task.completed && (!showArchived || task.archived);
     return true;
   });
 
@@ -89,9 +126,9 @@ export default function App() {
       <form onSubmit={handleSubmit} className="new-action-form">
         <div className="new-action-form-row">
           <label htmlFor="task">Task List</label>
-          <input value={newTask} onChange={e => setNewTask(e.target.value)} type="text" id='task' />
+          <input value={newTask} onChange={e => setNewTask(e.target.value)} type="text" id="task" />
         </div>
-        <button className='btn'>Add Task</button>
+        <button className="btn">Add Task</button>
       </form>
 
       {/* Task Filter Buttons */}
@@ -101,13 +138,24 @@ export default function App() {
         <button onClick={() => setFilter('remaining')}>Remaining Tasks</button>
       </div>
 
+      {/* Task Sorting Dropdown */}
+      <div className="task-sort-dropdown">
+        <label htmlFor="sort">Sort By:</label>
+        <select id="sort" value={sort} onChange={e => setSort(e.target.value)}>
+          <option value="none">None</option>
+          <option value="priority">Priority</option>
+          <option value="dueDate">Due Date</option>
+          <option value="creationDate">Creation Date</option>
+        </select>
+      </div>
+
       {/* Task List */}
-      <h1 className='header'>Task List</h1>
+      <h1 className="header">Task List</h1>
       <p>Completed Tasks: {completedTasksCount}</p>
-      <ul className='task-list'>
+      <ul className="task-list">
         {filteredTasks.length === 0 && "There are currently no tasks to display."}
         {filteredTasks.map(task => (
-          <li key={task.id}>
+          <li key={task.id} className={task.priority}>
             <label>
               <input
                 type="checkbox"
@@ -116,22 +164,33 @@ export default function App() {
               />
               {task.title}
             </label>
-            <button className='btn btn-danger' onClick={() => handleDeleteTask(task.id)}>
-              <i className='fas fa-trash'></i>
+            <button className="btn btn-danger" onClick={() => handleDeleteTask(task.id)}>
+              <i className="fas fa-trash"></i>
             </button>
-            <button className='btn btn-edit' onClick={() => {
-              const newTitle = prompt('Edit Task:', task.title);
-              if (newTitle !== null && newTitle.trim() !== '') {
-                handleEditTask(task.id, newTitle);
-              }
-            }}>
+            <button
+              className="btn btn-edit"
+              onClick={() => {
+                const newTitle = prompt('Edit Task:', task.title);
+                if (newTitle !== null && newTitle.trim() !== '') {
+                  handleEditTask(task.id, newTitle);
+                }
+              }}
+            >
               Edit
             </button>
             <input
               type="date"
               value={task.dueDate || ''}
-              onChange={(e) => handleDueDateChange(task.id, e.target.value)}
+              onChange={e => handleDueDateChange(task.id, e.target.value)}
             />
+            <select
+              value={task.priority}
+              onChange={e => handlePriorityChange(task.id, e.target.value)}
+            >
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+            </select>
           </li>
         ))}
       </ul>
