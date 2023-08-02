0888e1868038b4f3b3d23e2596cf96bc06a431a6
DMould123 <95115610+DMould123@users.noreply.github.com>
2023-08-02 13:27:18 +0200

    Added a archive option and an undo and redo button

--- src/App.jsx
+++ src/App.jsx
@@ -5,93 +5,308 @@ export default function App() {
   const [newTask, setNewTask] = useState('');
   const [tasks, setTasks] = useState([]);
   const [filter, setFilter] = useState('all');
-  const [sort, setSort] = useState('none'); // Sorting state, options: none, priority, dueDate, creationDate
+  const [sort, setSort] = useState('none');
   const [showArchived, setShowArchived] = useState(false);
+  const [archivedTasks, setArchivedTasks] = useState([]);
+  const [actionHistory, setActionHistory] = useState([]);
+  const [currentActionIndex, setCurrentActionIndex] = useState(-1);
 
-  // Load tasks from LocalStorage on app startup
+  // Load tasks and archived tasks from LocalStorage on app startup
   useEffect(() => {
     const storedTasks = localStorage.getItem('tasks');
     if (storedTasks) {
       setTasks(JSON.parse(storedTasks));
     }
+
+    const storedArchivedTasks = localStorage.getItem('archivedTasks');
+    if (storedArchivedTasks) {
+      setArchivedTasks(JSON.parse(storedArchivedTasks));
+    }
   }, []);
 
-  // Save tasks to LocalStorage whenever tasks change
+  // Save tasks and archived tasks to LocalStorage whenever they change
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }, [tasks]);
 
+  useEffect(() => {
+    localStorage.setItem('archivedTasks', JSON.stringify(archivedTasks));
+  }, [archivedTasks]);
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (newTask.trim() === '') return;
 
-    setTasks(ongoingTasks => {
-      return [
-        ...ongoingTasks,
-        {
-          id: crypto.randomUUID(),
-          title: newTask,
-          completed: false,
-          dueDate: null,
-          priority: 'medium', // default priority is medium
-        },
-      ];
+    const newTaskObject = {
+      id: crypto.randomUUID(),
+      title: newTask,
+      completed: false,
+      dueDate: null,
+      priority: 'medium', // default priority is medium
+    };
+
+    setTasks((ongoingTasks) => {
+      return [...ongoingTasks, newTaskObject];
     });
 
+    setActionHistory((history) => {
+      const newHistory = history.slice(0, currentActionIndex + 1);
+      return [...newHistory, { type: 'add', taskId: newTaskObject.id }];
+    });
+
+    setCurrentActionIndex((index) => index + 1);
     setNewTask(''); // clear the input field after adding the task
   }
 
   function handleToggleComplete(taskId) {
-    setTasks(tasks => {
-      return tasks.map(task => {
+    setTasks((currentTasks) => {
+      const updatedTasks = currentTasks.map((task) => {
         if (task.id === taskId) {
           return { ...task, completed: !task.completed };
         } else {
           return task;
         }
       });
+
+      setActionHistory((history) => {
+        const newHistory = history.slice(0, currentActionIndex + 1);
+        return [...newHistory, { type: 'toggleComplete', taskId }];
+      });
+
+      setCurrentActionIndex((index) => index + 1);
+      return updatedTasks;
     });
   }
 
   function handleDeleteTask(taskId) {
-    setTasks(ongoingTasks => {
-      return ongoingTasks.filter(task => task.id !== taskId);
+    setTasks((currentTasks) => {
+      const updatedTasks = currentTasks.filter((task) => task.id !== taskId);
+
+      setActionHistory((history) => {
+        const newHistory = history.slice(0, currentActionIndex + 1);
+        return [...newHistory, { type: 'delete', task }];
+      });
+
+      setCurrentActionIndex((index) => index + 1);
+      return updatedTasks;
     });
   }
 
   function handleEditTask(taskId, newTitle) {
-    setTasks(tasks => {
-      return tasks.map(task => {
+    setTasks((currentTasks) => {
+      const updatedTasks = currentTasks.map((task) => {
         if (task.id === taskId) {
           return { ...task, title: newTitle };
         } else {
           return task;
         }
       });
+
+      setActionHistory((history) => {
+        const newHistory = history.slice(0, currentActionIndex + 1);
+        return [...newHistory, { type: 'edit', taskId, newTitle }];
+      });
+
+      setCurrentActionIndex((index) => index + 1);
+      return updatedTasks;
     });
   }
 
   function handleDueDateChange(taskId, newDueDate) {
-    setTasks(tasks => {
-      return tasks.map(task => {
+    setTasks((currentTasks) => {
+      const updatedTasks = currentTasks.map((task) => {
         if (task.id === taskId) {
           return { ...task, dueDate: newDueDate };
         } else {
           return task;
         }
       });
+
+      setActionHistory((history) => {
+        const newHistory = history.slice(0, currentActionIndex + 1);
+        return [...newHistory, { type: 'dueDateChange', taskId, newDueDate }];
+      });
+
+      setCurrentActionIndex((index) => index + 1);
+      return updatedTasks;
     });
   }
 
   function handlePriorityChange(taskId, newPriority) {
-    setTasks(tasks => {
-      return tasks.map(task => {
+    setTasks((currentTasks) => {
+      const updatedTasks = currentTasks.map((task) => {
         if (task.id === taskId) {
           return { ...task, priority: newPriority };
         } else {
           return task;
         }
       });
+
+      setActionHistory((history) => {
+        const newHistory = history.slice(0, currentActionIndex + 1);
+        return [...newHistory, { type: 'priorityChange', taskId, newPriority }];
+      });
+
+      setCurrentActionIndex((index) => index + 1);
+      return updatedTasks;
     });
+  }
+
+  function handleArchiveCompletedTasks() {
+    const completedTasks = tasks.filter((task) => task.completed);
+    setTasks((currentTasks) => currentTasks.filter((task) => !task.completed));
+    setArchivedTasks((currentArchivedTasks) => [...currentArchivedTasks, ...completedTasks]);
+
+    setActionHistory((history) => {
+      const newHistory = history.slice(0, currentActionIndex + 1);
+      return [...newHistory, { type: 'archiveCompleted' }];
+    });
+
+    setCurrentActionIndex((index) => index + 1);
+  }
+
+  function handleUndo() {
+    if (currentActionIndex >= 0) {
+      const actionToUndo = actionHistory[currentActionIndex];
+      setCurrentActionIndex((index) => index - 1);
+
+      switch (actionToUndo.type) {
+        case 'add':
+          setTasks((currentTasks) => currentTasks.filter((task) => task.id !== actionToUndo.taskId));
+          break;
+        case 'toggleComplete':
+          setTasks((currentTasks) => {
+            const updatedTasks = currentTasks.map((task) => {
+              if (task.id === actionToUndo.taskId) {
+                return { ...task, completed: !task.completed };
+              } else {
+                return task;
+              }
+            });
+            return updatedTasks;
+          });
+          break;
+        case 'delete':
+          setTasks((currentTasks) => [...currentTasks, actionToUndo.task]);
+          break;
+        case 'edit':
+          setTasks((currentTasks) => {
+            const updatedTasks = currentTasks.map((task) => {
+              if (task.id === actionToUndo.taskId) {
+                return { ...task, title: actionToUndo.newTitle };
+              } else {
+                return task;
+              }
+            });
+            return updatedTasks;
+          });
+          break;
+        case 'dueDateChange':
+          setTasks((currentTasks) => {
+            const updatedTasks = currentTasks.map((task) => {
+              if (task.id === actionToUndo.taskId) {
+                return { ...task, dueDate: actionToUndo.newDueDate };
+              } else {
+                return task;
+              }
+            });
+            return updatedTasks;
+          });
+          break;
+        case 'priorityChange':
+          setTasks((currentTasks) => {
+            const updatedTasks = currentTasks.map((task) => {
+              if (task.id === actionToUndo.taskId) {
+                return { ...task, priority: actionToUndo.newPriority };
+              } else {
+                return task;
+              }
+            });
+            return updatedTasks;
+          });
+          break;
+        case 'archiveCompleted':
+          setArchivedTasks((currentArchivedTasks) => {
+            const lastCompletedTasks = tasks.filter((task) => task.completed);
+            return currentArchivedTasks.slice(0, currentArchivedTasks.length - lastCompletedTasks.length);
+          });
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  function handleRedo() {
+    if (currentActionIndex < actionHistory.length - 1) {
+      setCurrentActionIndex((index) => index + 1);
+      const actionToRedo = actionHistory[currentActionIndex + 1];
+
+      switch (actionToRedo.type) {
+        case 'add':
+          setTasks((currentTasks) => currentTasks.filter((task) => task.id !== actionToRedo.taskId));
+          break;
+        case 'toggleComplete':
+          setTasks((currentTasks) => {
+            const updatedTasks = currentTasks.map((task) => {
+              if (task.id === actionToRedo.taskId) {
+                return { ...task, completed: !task.completed };
+              } else {
+                return task;
+              }
+            });
+            return updatedTasks;
+          });
+          break;
+        case 'delete':
+          setTasks((currentTasks) => [...currentTasks, actionToRedo.task]);
+          break;
+        case 'edit':
+          setTasks((currentTasks) => {
+            const updatedTasks = currentTasks.map((task) => {
+              if (task.id === actionToRedo.taskId) {
+                return { ...task, title: actionToRedo.newTitle };
+              } else {
+                return task;
+              }
+            });
+            return updatedTasks;
+          });
+          break;
+        case 'dueDateChange':
+          setTasks((currentTasks) => {
+            const updatedTasks = currentTasks.map((task) => {
+              if (task.id === actionToRedo.taskId) {
+                return { ...task, dueDate: actionToRedo.newDueDate };
+              } else {
+                return task;
+              }
+            });
+            return updatedTasks;
+          });
+          break;
+        case 'priorityChange':
+          setTasks((currentTasks) => {
+            const updatedTasks = currentTasks.map((task) => {
+              if (task.id === actionToRedo.taskId) {
+                return { ...task, priority: actionToRedo.newPriority };
+              } else {
+                return task;
+              }
+            });
+            return updatedTasks;
+          });
+          break;
+        case 'archiveCompleted':
+          setArchivedTasks((currentArchivedTasks) => {
+            const lastCompletedTasks = tasks.filter((task) => task.completed);
+            return [...currentArchivedTasks, ...lastCompletedTasks];
+          });
+          break;
+        default:
+          break;
+      }
+    }
   }
 
   // Function to handle task sorting
@@ -111,7 +326,7 @@ export default function App() {
   });
 
   // Function to handle task filtering
-  const filteredTasks = sortedTasks.filter(task => {
+  const filteredTasks = sortedTasks.filter((task) => {
     if (filter === 'all') return true;
     if (filter === 'completed') return task.completed && (!showArchived || task.archived);
     if (filter === 'remaining') return !task.completed && (!showArchived || task.archived);
@@ -119,14 +334,14 @@ export default function App() {
   });
 
   // Count of completed tasks
-  const completedTasksCount = tasks.filter(task => task.completed).length;
+  const completedTasksCount = tasks.filter((task) => task.completed).length;
 
   return (
     <div className="app-container">
       <form onSubmit={handleSubmit} className="new-action-form">
         <div className="new-action-form-row">
           <label htmlFor="task">Task List</label>
-          <input value={newTask} onChange={e => setNewTask(e.target.value)} type="text" id="task" />
+          <input value={newTask} onChange={(e) => setNewTask(e.target.value)} type="text" id="task" />
         </div>
         <button className="btn">Add Task</button>
       </form>
@@ -136,12 +351,13 @@ export default function App() {
         <button onClick={() => setFilter('all')}>All Tasks</button>
         <button onClick={() => setFilter('completed')}>Completed Tasks</button>
         <button onClick={() => setFilter('remaining')}>Remaining Tasks</button>
+        <button onClick={handleArchiveCompletedTasks}>Archive Completed</button>
       </div>
 
       {/* Task Sorting Dropdown */}
       <div className="task-sort-dropdown">
         <label htmlFor="sort">Sort By:</label>
-        <select id="sort" value={sort} onChange={e => setSort(e.target.value)}>
+        <select id="sort" value={sort} onChange={(e) => setSort(e.target.value)}>
           <option value="none">None</option>
           <option value="priority">Priority</option>
           <option value="dueDate">Due Date</option>
@@ -153,8 +369,8 @@ export default function App() {
       <h1 className="header">Task List</h1>
       <p>Completed Tasks: {completedTasksCount}</p>
       <ul className="task-list">
-        {filteredTasks.length === 0 && "There are currently no tasks to display."}
-        {filteredTasks.map(task => (
+        {filteredTasks.length === 0 && <li>No tasks to display.</li>}
+        {filteredTasks.map((task) => (
           <li key={task.id} className={task.priority}>
             <label>
               <input
@@ -181,11 +397,11 @@ export default function App() {
             <input
               type="date"
               value={task.dueDate || ''}
-              onChange={e => handleDueDateChange(task.id, e.target.value)}
+              onChange={(e) => handleDueDateChange(task.id, e.target.value)}
             />
             <select
               value={task.priority}
-              onChange={e => handlePriorityChange(task.id, e.target.value)}
+              onChange={(e) => handlePriorityChange(task.id, e.target.value)}
             >
               <option value="low">Low</option>
               <option value="medium">Medium</option>
@@ -194,6 +410,38 @@ export default function App() {
           </li>
         ))}
       </ul>
+
+      {/* Archive List */}
+      <h1 className="header">Archived Tasks</h1>
+      <ul className="task-list">
+        {archivedTasks.length === 0 && <li>No archived tasks.</li>}
+        {archivedTasks.map((task) => (
+          <li key={task.id} className={task.priority}>
+            {task.title}
+            <button
+              className="btn btn-restore"
+              onClick={() => {
+                setArchivedTasks((currentArchivedTasks) =>
+                  currentArchivedTasks.filter((archivedTask) => archivedTask.id !== task.id)
+                );
+                setTasks((currentTasks) => [...currentTasks, task]);
+              }}
+            >
+              Restore
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {/* Undo and Redo Buttons */}
+      <div className="undo-redo-buttons">
+        <button onClick={handleUndo} disabled={currentActionIndex < 0}>
+          Undo
+        </button>
+        <button onClick={handleRedo} disabled={currentActionIndex >= actionHistory.length - 1}>
+          Redo
+        </button>
+      </div>
     </div>
   );
 }
